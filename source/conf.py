047461c4d7b51daae7200efb5dc584a84a7e9e01
Albert Brouwer <brouwer@iiasa.ac.at>
2020-06-19 08:06:49 +0200

    Require Sphinx 3.1 to avoid link breakage when generating on older versions

--- source/conf.py
+++ source/conf.py
@@ -33,7 +33,7 @@ release = ''
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = '1.6'
+needs_sphinx = '3.1'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
