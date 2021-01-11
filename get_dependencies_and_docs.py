890f2ae2e8145c15d186026c643420aa91f52af0
Albert Brouwer <brouwer@iiasa.ac.at>
2021-01-11 16:38:54 +0100

    Prevent source/images directory from being deleted

--- get_dependencies_and_docs.py
+++ get_dependencies_and_docs.py
@@ -377,7 +377,7 @@ if __name__ == '__main__':
         # Delete any previously-generated subdirectories of the source directory
         for dirpath, dirs, files in os.walk('source'):
             for d in dirs:
-                if d not in  ['_static', '_templates']:
+                if d not in  ['_static', '_templates', 'images']:
                     shutil.rmtree(os.path.join(dirpath, d))
         # Generate the index
         print("----------------------- Generating reStructuredText index")
