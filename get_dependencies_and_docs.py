7967b96e5a61365c021ed852cd0ff4de5455745d
Albert Brouwer <brouwer@iiasa.ac.at>
2021-01-11 16:57:46 +0100

    Move images to _images

--- get_dependencies_and_docs.py
+++ get_dependencies_and_docs.py
@@ -377,7 +377,7 @@ if __name__ == '__main__':
         # Delete any previously-generated subdirectories of the source directory
         for dirpath, dirs, files in os.walk('source'):
             for d in dirs:
-                if d not in  ['_static', '_templates', 'images']:
+                if d not in  ['_static', '_templates', '_images']:
                     shutil.rmtree(os.path.join(dirpath, d))
         # Generate the index
         print("----------------------- Generating reStructuredText index")
